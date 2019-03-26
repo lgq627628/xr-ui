85bdf852bb24fa794db09197752869538b9f2100
blake <blake@meishakeji.com>
2019-03-26 17:29:40 +0800

    feat: 新建build目录增加自动化脚本代码

---
+++ build/build-lib.js
@@ -0,0 +1,24 @@
+/**
+ * Build npm lib
+ */
+
+// 这个库能够让我们在js文件中执行shell命令
+// 基于node的一层命令封装插件
+const shell = require('shelljs')
+// 优雅的控制台日志输出工具
+const signale = require('signale')
+
+const { Signale } = signale
+const tasks = [
+  'build:entry',
+  'lib'
+]
+
+tasks.forEach(task => {
+  signale.start(task)
+
+  const interactive = new Signale({ interactive: true })
+  interactive.pending(task)
+  shell.exec(`npm run ${task} --silent`)
+  interactive.success(task)
+})
--- packages/index.js
+++ packages/index.js
@@ -1,27 +1,28 @@
-import XrTest from './test'
-// 存储组件列表
-const components = [
-  XrTest
-]
-// 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
-const install = function (Vue) {
-  // 判断是否安装
-  if (install.installed) return
-  install.installed = true
-  // 遍历注册全局组件
-  components.map(component => Vue.component(component.name, component))
-  // 下面这个写法也可以
-  // components.map(component => Vue.use(component))
-}
-
-// 判断是否是直接引入文件
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
-
-export default {
-  // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
-  install,
-  // 以下是具体的组件列表
-  ...components
-}
+/* eslint-disable */
+// This file is auto gererated by build/build-entry.js
+    import XrTest from './test'
+    const version = '0.3.0'
+    const components = [
+      XrTest
+    ]
+    const install = Vue => {
+      if (install.installed) return
+      install.installed = true
+      components.forEach(Component => {
+        Vue.use(Component)
+      })
+    };
+    /* istanbul ignore if */
+    if (typeof window !== 'undefined' && window.Vue) {
+      install(window.Vue)
+    }
+    export {
+      install,
+      version,
+      XrTest
+    }
+    export default {
+      install,
+      version
+    }
+    
